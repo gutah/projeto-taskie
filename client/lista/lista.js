27f5109d893dbf5f29208825537553bd137ef8da
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-01-21 15:18:58 -0200

    Iniciando conexao com bacno de dados e colocando plugin bootstrap

--- client/lista/lista.js
+++ client/lista/lista.js
@@ -1,10 +1,6 @@
 Template.lista.helpers({
 
   tarefas : function () {
-    return [
-      {nome: "Item 1"},
-      {nome: "Item 2"},
-      {nome: "Algo"}
-    ];
+    return Tarefas.find({});
   }
 })
