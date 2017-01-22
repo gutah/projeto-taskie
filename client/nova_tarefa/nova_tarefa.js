9d0d77d1bfdec992b0e69fc3b3f60ef4661cbdbd
Gustavo Maciel <gustavo_nmaciel@hotmail.com>
2017-01-22 17:49:59 -0200

    Inserindo Tarefa Foi inserido a funcioanalidade de inserir tarefa, e inserindo o evento pra isso. A conexao com o banco também foi realizada.

---
+++ client/nova_tarefa/nova_tarefa.js
@@ -0,0 +1,13 @@
+Template.nova_tarefa.events({
+
+	"submit form": function(e, template){
+		e.preventDefault();
+		
+		var input = $("#tarefa");
+		var nome = input.val();
+
+		Tarefas.insert({nome: nome, data: new Date()});
+		input.val("");
+	}
+
+})
